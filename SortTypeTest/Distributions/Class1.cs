154fdb31cd8ade3be267f51c66ac426dd3f04037
alopinska <lopinska.aleksandra@gmail.com>
2018-06-28 21:44:01 +0200

    Sorting lists of variuosly distributed data with four algorithms - time comparison.

---
+++ SortTypeTest/Distributions/Class1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Distributions
+    {
+        public static void IncreasingDistribution(int[] t, int length)
+        {
+            for (int i = 0; i < length; i++) t[i] = i;
+        }
+        public static void DecreasingDistribution(int[] t, int length)
+        {
+            for (int i = 0; i < length; i++) t[i] = length - i;
+        }
+        public static void ConstantDistribution(int[] t, int length)
+        {
+            for (int i = 0; i < length; i++) t[i] = 5;
+        }
+        public static void RandomDistribution(int[] t, int length)
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            for (int i = 0; i < length; i++) t[i] = rnd.Next(length);
+        }
+        public static void VShapeDistribution(int[] t, int length)
+        {
+            for (int i = 0; i < length / 2; i++) t[i] = length - (2 * i + 1);
+            for (int i = length / 2; i < length; i++) t[i] = 2 * i - length;
+        }
+    }
+    public class SortType
+    {
+        public static void InsertionSort(int[] t, int currentLength)
+        {
+            int i, j, tmp;
+            for (i = 1; i < currentLength + 1; i++)
+            {
+                j = i;
+                tmp = t[j];
+                while ((j > 0) && tmp < t[j - 1])
+                {
+                    t[j] = t[j - 1];
+                    t[j - 1] = tmp;
+                    j--;
+                }
+            }
+        }
+        public static void SelectionSort(int[] t, int currentLength)
+        {
+            int i, j, min, tmp;
+            for (i = 0; i < currentLength; i++)
+            {
+                min = i;
+                for (j = i + 1; j < currentLength + 1; j++)
+                {
+                    if (t[j] < t[min]) min = j;
+                }
+                if (min != i)
+                {
+                    tmp = t[i];
+                    t[i] = t[min];
+                    t[min] = tmp;
+                }
+            }
+        }
+        public static void CocktailSort(int[] t, int currentLength)
+        {
+            int i, j, tmp;
+            bool sorted = false;
+            for (i = 0; i < currentLength / 2; i++)
+            {
+                for (j = i; j < currentLength - i - 1; j++)
+                {
+                    if (t[j] > t[j + 1])
+                    {
+                        tmp = t[j];
+                        t[j] = t[j + 1];
+                        t[j + 1] = tmp;
+                        sorted = true;
+                    }
+                }
+                for (j = currentLength - 2 - i; j > i; j--)
+                {
+                    if (t[j] < t[j - 1])
+                    {
+                        tmp = t[j];
+                        t[j] = t[j - 1];
+                        t[j - 1] = tmp;
+                        sorted = true;
+                    }
+                }
+                if (sorted == false) break;
+            }
+        }
+        public static void HeapSort(int[] t, int currentLength)
+        {
+            MakeHeap(t, currentLength);
+            int n = currentLength;
+            while (n > 1)
+            {
+                int root = t[0];
+                t[0] = t[n - 1];
+                t[n - 1] = root;
+                n = n - 1;
+                FixHeap(t, 0, n);
+            }
+        }
+        static void MakeHeap(int[] t, int currentLength)
+        {
+            for (int i = currentLength / 2 - 1; i >= 0; i--) FixHeap(t, i, currentLength);
+        }
+        static void FixHeap(int[] t, int node, int length)
+        {
+            int i = node;
+            int j = 2 * i + 1;
+            int k = 2 * i + 2;
+
+            if (j < length && t[j] > t[i]) i = j;
+            if (k < length && t[k] > t[i]) i = k;
+            if (i != node)
+            {
+                int tmp = t[node];
+                t[node] = t[i];
+                t[i] = tmp;
+                FixHeap(t, i, length);
+            }
+        }
+    }
+}
---
+++ SortTypeTest/SortTypeTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortTypeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sortOp = 10;
+            StreamWriter file = new StreamWriter("SortTest.txt");
+            Stopwatch time = new Stopwatch();
+            int[] array = new int[200000];
+            Action<int[], int>[] distributions = { Methods.Distributions.IncreasingDistribution, Methods.Distributions.DecreasingDistribution, Methods.Distributions.ConstantDistribution, Methods.Distributions.RandomDistribution, Methods.Distributions.VShapeDistribution };
+            Action<int[], int>[] sorters = { Methods.SortType.InsertionSort, Methods.SortType.SelectionSort, Methods.SortType.CocktailSort, Methods.SortType.HeapSort };
+            foreach (var distribution in distributions)
+            {
+                for (int i = 50000; i < array.Length; i += 10000)
+                {
+                    foreach (var sorter in sorters)
+                    {
+                        double elapsedTime = 0;
+                        for (int j = 0; j < sortOp; j++)
+                        {
+                            distribution(array, i);
+                            time.Start();
+                            sorter(array, i);
+                            time.Stop();
+                            double iterationTime = time.Elapsed.TotalSeconds;
+                            elapsedTime += iterationTime;
+                        }
+                        double SecondsMid = elapsedTime / sortOp;
+                        Console.WriteLine("{0}: {1}", i, SecondsMid.ToString("F4"));
+                        file.WriteLine("{0}: {1}", i, SecondsMid.ToString("F4"));
+                        time.Reset();
+                    }
+                }
+                Console.WriteLine();
+                file.WriteLine();
+            }
+            file.Close();
+        }        
+    }
+}
